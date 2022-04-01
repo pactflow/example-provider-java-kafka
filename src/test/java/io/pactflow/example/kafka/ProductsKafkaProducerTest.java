eb77c6caa29af31129c602a093a734a6296adffc
Yousaf Nabi <yousafn@gmail.com>
2022-04-01 17:20:15 +0100

    chore: update imports to support consumer version selectors

--- src/test/java/io/pactflow/example/kafka/ProductsKafkaProducerTest.java
+++ src/test/java/io/pactflow/example/kafka/ProductsKafkaProducerTest.java
@@ -4,13 +4,12 @@ import au.com.dius.pact.core.model.Interaction;
 import au.com.dius.pact.core.model.Pact;
 import au.com.dius.pact.provider.MessageAndMetadata;
 import au.com.dius.pact.provider.PactVerifyProvider;
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit5.AmpqTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
 
 import java.util.HashMap;
@@ -27,7 +26,8 @@ import org.springframework.messaging.Message;
 @Provider("pactflow-example-provider-java-kafka")
 @PactBroker(scheme = "https", host = "${PACT_BROKER_HOST}",
         consumerVersionSelectors = {@VersionSelector(tag = "master"), @VersionSelector(tag = "prod")},
-        authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))public class ProductsKafkaProducerTest {
+        authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
+  public class ProductsKafkaProducerTest {
   private static final Logger LOGGER = LoggerFactory.getLogger(ProductsKafkaProducerTest.class);
 
   @TestTemplate
